e7aa1a79346411a7cee1fe9dc50badb9a3931dcd
Aaron <Aaron@Aaron-PC>
2013-10-31 16:45:13 +0100

    bei mir funktioniert es in nem anderen Ordner. hier direkt nicht.

--- Blatt1 Aufgabe 1/Blatt1Aufgabe1.cpp
+++ Blatt1 Aufgabe 1/Blatt1Aufgabe1.cpp
@@ -6,11 +6,18 @@
  *      Author: felix
  */
 
+#include <iostream>
+#include "math.h"
+using namespace std;
 
-
-
-#include<iostream>
-
-int main(){
+int main(void){
+	float wert1, wert2;
+	cout << "gib n wert ein" << endl;
+	cin>>wert1;
+	cout << "gib n größeren wert ein" << endl;
+	cin>>wert2;
+	for(int i = int(ceil(wert1)) ;i < floor(wert2); i++)
+		cout << i << "\n";
 	return 0;
 }
+
