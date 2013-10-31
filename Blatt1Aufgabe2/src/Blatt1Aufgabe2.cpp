7ac233eb60e53dcd4e63307011497a40cd9f67a5
Markus <Markus@MarkusPC>
2013-10-31 17:40:44 +0100

    neu A2

---
+++ Blatt1Aufgabe2/src/Blatt1Aufgabe2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Blatt1Aufgabe2.cpp
+// Author      : AFM
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!!!" << endl; // prints Hello World!!!
+	return 0;
+}
