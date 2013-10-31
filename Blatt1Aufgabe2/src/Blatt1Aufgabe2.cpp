99ad9f4b0164f392cf08b8a6ab11c3e3f2461fe2
Aaron <Aaron@Aaron-PC>
2013-10-31 18:39:44 +0100

    help

--- Blatt1Aufgabe2/src/Blatt1Aufgabe2.cpp
+++ Blatt1Aufgabe2/src/Blatt1Aufgabe2.cpp
@@ -1,15 +1,32 @@
-//============================================================================
-// Name        : Blatt1Aufgabe2.cpp
-// Author      : AFM
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+/*
+ * Programme2.cpp
+ *
+ *  Created on: 31.10.2013
+ *      Author: Aaron
+ */
 
 #include <iostream>
+
 using namespace std;
 
-int main() {
-	cout << "Hello World!!!" << endl; // prints Hello World!!!
+int FibonacciZahl(int in){
+	if(in > 1){
+		return (FibonacciZahl(in - 1) + FibonacciZahl(in - 2));
+	}
+	else if (in == 1)
+		return 1;
+	else
+		return 0;
+};
+
+int main(){
+	int eingabe;
+	cout<<"Geben Sie eine natürliche nichtnegative Zahl ein"<<endl;
+	cin >>eingabe;
+	if (eingabe < 0)
+		cout<<"Eingabe nicht erlaubt";
+	else
+	    cout<<"Die Fibonacci-Zahl zu " <<eingabe<<" ist "<<FibonacciZahl(eingabe)<<endl;
+
 	return 0;
 }
