5b74c6c1ca873a410320099df2ae706c7c4150bb
Markus <Markus@MarkusPC>
2013-11-03 15:07:51 +0100

    orginal daten (bis auf main)

---
+++ Blatt1A4/src/Blatt1A4.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Blatt1A4.cpp
+// Author      : AFM
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+#include "IO.hpp"
+
+int main() {
+	cout << "Blatt1A4" << endl; // prints Hello World!!!
+	return 0;
+}
