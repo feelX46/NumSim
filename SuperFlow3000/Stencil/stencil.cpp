1b5111ab44f046f8921ab49fbe234ee0cbab7f97
ischifx <ischifx@visgs06.visus.uni-stuttgart.de>
2013-11-06 15:30:31 +0100

    Neue Ordnerstruktur - COMPILED NICHT wegen Konstruktor Stencil

---
+++ SuperFlow3000/Stencil/stencil.cpp
@@ -0,0 +1,13 @@
+/*
+ * stencil.cpp
+ *
+ *  Created on: Nov 6, 2013
+ *      Author: ischifx
+ */
+
+#include "stencil.h"
+
+Stencil::Stencil(int stencilwidth_input, const PointType& h_input) {
+	stencilwidth = stencilwidth_input;
+	h = h_input;
+}
---
+++ SuperFlow3000/Stencil/stencil.h
@@ -0,0 +1,27 @@
+/*
+ * stencil.h
+ *
+ *  Created on: Nov 6, 2013
+ *      Author: ischifx
+ */
+
+#ifndef STENCIL_H_
+#define STENCIL_H_
+
+
+#include"../Misc/typedef.h"
+
+class Stencil {
+public:
+
+	Stencil(int stencilwidth_input, const PointType& h_input);
+	//void ~Stencil();
+
+	//StencilType stencil;
+	int stencilwidth;
+	const PointType& h;
+};
+
+
+
+#endif /* STENCIL_H_ */
