39aac4c2f4d0ce177ecddfef199ce59bd34654af
Markus <Markus@MarkusPC>
2013-11-08 19:23:58 +0100

    implemented all functions in gridfunction

--- SuperFlow3000/Grid/gridfunction.cpp
+++ SuperFlow3000/Grid/gridfunction.cpp
@@ -6,7 +6,8 @@
  */
 
 #include "gridfunction.h"
-
+#include <iostream>
+//1
 GridFunction::GridFunction(int DimX, int DimY){
 	 gridfunction= new RealType*[DimX];
 	 for (int i = 0; i < DimX; i++){
@@ -16,6 +17,7 @@ GridFunction::GridFunction(int DimX, int DimY){
 	 griddimension[2] = DimY;
 }
 
+//2
 GridFunction::GridFunction(const MultiIndexType griddimension_input) : griddimension(griddimension_input){
 	 gridfunction= new RealType*[griddimension[1]];
 	 for (int i = 0; i < griddimension[1]; i++){
@@ -23,6 +25,7 @@ GridFunction::GridFunction(const MultiIndexType griddimension_input) : griddimen
 	 }
 }
 
+//3
 GridFunction::~GridFunction(){
 	for(IndexType delCol = 0; delCol < griddimension[1]; delCol++)
 		{
@@ -31,65 +34,118 @@ GridFunction::~GridFunction(){
 		delete [] gridfunction;
 	}
 
-
+//4
 GridFunctionType GridFunction::GetGridFunction() const{
 	return gridfunction;
 }
-
+//new
 RealType GridFunction::GetGridFunction(const MultiIndexType& index){
 	return gridfunction[index[1]][index[2]];
 }
 
-
+//new
 MultiIndexType GridFunction::GetGridDimension(){
 	return griddimension;
 }
 
-
+//5
 void GridFunction::SetGridFunction (const MultiIndexType& begin, const MultiIndexType& end,
 		RealType value){
-	for (int i = begin[2]; i++; i<end[2]){
-		for (int j = begin[1]; j++; j<end[1]){
+	for (IndexType i = begin[2];i<end[2]; i++){
+		for (IndexType j = begin[1]; j<end[1]; j++){
 			gridfunction[i][j] = value;
 		}
 	}
 }
 
-
+//new
 void GridFunction::SetGridFunction (const IndexType& i, const IndexType& j,
 		RealType value){
 		gridfunction[i][j] = value;
 }
 
+//6
+void GridFunction::SetGridFunction (const MultiIndexType& begin, const MultiIndexType& end,RealType factor,
+			MultiIndexType& offset){
+	for (IndexType i = begin[2];i<end[2]; i++){
+		for (IndexType j = begin[1]; j<end[1]; j++){
+			gridfunction[i][j] = factor * gridfunction[i+offset[0]][j+offset[1]];
+		}
+	}
+}
+
+//8
 void GridFunction::SetGridFunction (const MultiIndexType& begin, const MultiIndexType& end,
 		RealType factor, GridFunctionType& sourcegridfunction){
+	for (IndexType i = begin[2];i<end[2]; i++){
+		for (IndexType j = begin[1]; j<end[1]; j++){
+			gridfunction[i][j] = factor * sourcegridfunction[i][j];
+		}
+	}
 }
 
-
+//9
 void GridFunction::SetGridFunction (const MultiIndexType& begin, const MultiIndexType& end,
 		RealType factor, GridFunctionType& sourcegridfunction, MultiIndexType& offset){
-
+	for (IndexType i = begin[2];i<end[2]; i++){
+		for (IndexType j = begin[1]; j<end[1]; j++){
+			gridfunction[i][j] = factor * sourcegridfunction[i+offset[0]][j+offset[1]];
+		}
+	}
 }
 
+//~10
+void GridFunction::SetGridFunction (const MultiIndexType& begin, const MultiIndexType& end,
+		RealType factor,GridFunctionType& sourcegridfunction, RealType constant){
+	for (IndexType i = begin[2];i<end[2]; i++){
+		for (IndexType j = begin[1]; j<end[1]; j++){
+			gridfunction[i][j] = constant + factor * sourcegridfunction[i][j];
+		}
+	}
+}
 
+//~10
 void GridFunction::SetGridFunction (const MultiIndexType& begin, const MultiIndexType& end,
 		RealType factor,GridFunctionType& sourcegridfunction, MultiIndexType& offset,
 		RealType constant){
+	for (IndexType i = begin[2];i<end[2]; i++){
+		for (IndexType j = begin[1]; j<end[1]; j++){
+			gridfunction[i][j] = constant + factor * sourcegridfunction[i+offset[0]][j+offset[1]];
+		}
+	}
 }
 
-
+//7
 void GridFunction::ScaleGridFunction (const MultiIndexType& begin, const MultiIndexType& end,
 		RealType factor){
+	for (IndexType i = begin[2];i<end[2]; i++){
+		for (IndexType j = begin[1]; j<end[1]; j++){
+			gridfunction[i][j] = factor * gridfunction[i][j];
+		}
+	}
+
 }
 
-
+//11
 void GridFunction::AddToGridFunction (const MultiIndexType& begin, const MultiIndexType& end,
-		RealType factor, GridFunctionType& imagegridfunction){
-
+		RealType factor, GridFunctionType& sourcegridfunction){
+	for (IndexType i = begin[2];i<end[2]; i++){
+		for (IndexType j = begin[1]; j<end[1]; j++){
+			gridfunction[i][j] = gridfunction[i][j] + factor * sourcegridfunction[i][j];
+		}
+	}
 }
 
-
+//12
 RealType GridFunction::MaxValueGridFunction (const MultiIndexType& begin,
 		const MultiIndexType& end){
-	return 0;
+	IndexType max = 0;
+	for (IndexType i = begin[2];i<end[2]; i++){
+		for (IndexType j = begin[1]; j<end[1]; j++){
+			if (gridfunction[i][j] > max)
+				max = gridfunction[i][j];
+		}
+	}
+
+	return max;
 }
