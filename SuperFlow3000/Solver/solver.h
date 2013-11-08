f56c896475ccf41344a0c6c61d616af676e8ae70
Aaron <Aaron@Aaron-PC>
2013-11-08 13:27:56 +0100

    Header solver.h Sind private/public-Entscheidungen okay? muss IO::IO Reader; hier definiert werden?

---
+++ SuperFlow3000/Solver/solver.h
@@ -0,0 +1,35 @@
+/*
+ * solver.h
+ *
+ *  Created on: 08.11.2013
+ *      Author: Aaron
+ */
+
+#ifndef SOLVER_H_
+#define SOLVER_H_
+
+
+#include"../Misc/typedef.h"
+
+class Solver {
+public:
+    RealType computeResidual(GridFunctionType& sourcegridfunction,
+    						 GridFunctionType& rhs,
+    						 const PointType& h);
+
+    void SORCycle(GridFunction* gridfunction,
+    	    	  GridFunctionType& rhs,
+    	    	  const PointType& delta,
+    	    	  RealType omega);
+
+    Solver::Solver(void);
+
+    Solver::~Solver();
+
+private:
+
+};
+
+IO::IO Reader;
+
+#endif /* SOLVER_H_ */
