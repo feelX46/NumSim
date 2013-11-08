c045c5c393767e7adeaccb7c43684567b8c226a7
Markus <Markus@MarkusPC>
2013-11-08 15:10:40 +0100

    (leere) Funktionen in gridfunction eingebunden

--- SuperFlow3000/Grid/Gridfunction.cpp
+++ SuperFlow3000/Grid/Gridfunction.cpp
@@ -7,7 +7,6 @@
 
 #include "Gridfunction.h"
 
-//bliblbablub
 Gridfunction::Gridfunction(int DimX, int DimY){
 	 RealType *grid_data[DimY];
 	 for (int i=0; i < DimY; i++){
@@ -24,4 +23,49 @@ Gridfunction::Gridfunction(const MultiIndexType griddimension){
 
 Gridfunction::~Gridfunction(){
 
+}
+
+//! get grid data
+GridFunctionType Gridfunction::getGridfunction(){
+	return 0;
+}
+
+//! get dimension
+MultiIndexType Gridfunction::getGriddimension(){
+	return griddimension;
+}
+
+//! set grid data by
+void Gridfunction::SetGridFunction (const MultiIndexType& begin, const MultiIndexType& end,RealType value){
+}
+
+//!
+void Gridfunction::SetGridFunction (const MultiIndexType& begin, const MultiIndexType& end,
+		RealType factor, GridFunctionType& sourcegridfunction){
+}
+
+//!
+void Gridfunction::SetGridFunction (const MultiIndexType& begin, const MultiIndexType& end,
+		RealType factor, GridFunctionType& sourcegridfunction, MultiIndexType& offset){
+}
+
+//!
+void Gridfunction::SetGridFunction (const MultiIndexType& begin, const MultiIndexType& end,
+		RealType factor,GridFunctionType& sourcegridfunction, MultiIndexType& offset,
+		RealType constant){
+}
+
+//! scale grid
+void Gridfunction::ScaleGridFunction (const MultiIndexType& begin, const MultiIndexType& end, RealType factor){
+}
+
+//!
+void Gridfunction::AddToGridFunction (const MultiIndexType& begin, const MultiIndexType& end,RealType factor,
+		GridFunctionType& imagegridfunction){
+
+}
+
+//!
+RealType Gridfunction::MaxValueGridFunction (const MultiIndexType& begin, const MultiIndexType& end){
+	return 0;
 }
