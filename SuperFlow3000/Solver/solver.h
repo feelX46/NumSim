3928c89ae1ab9ebe13d578a2ced3186a363c0db2
Aaron <Aaron@Aaron-PC>
2013-11-08 14:22:32 +0100

    doxygen commentation trial

--- SuperFlow3000/Solver/solver.h
+++ SuperFlow3000/Solver/solver.h
@@ -14,17 +14,36 @@
 
 class Solver {
 public:
+	/*! Function to compute the global residual
+		   * @param sourcegridfunction ?The discretized solution
+		   * @param rhs The right hand side of the discretized local PDE
+		   * @param h ?what are these two RealTypes for?
+		   */
     RealType computeResidual(GridFunctionType& sourcegridfunction,
     						 GridFunctionType& rhs,
     						 const PointType& h);
+    //-------------------------------------------------------------------------------
 
-    void SORCycle(GridFunction* gridfunction,
+    /*! Function to compute the global residual
+    		   * @param gridfunction Pointer on the discretized solution
+    		   * @param rhs The right hand side of the discretized local PDE
+    		   * @param delta \delta_i are the gridwidths in x- and y-direction
+    		   * @param omega The \omega -parameter of the SOR-cycle
+    		   */
+    void SORCycle(Gridfunction* gridfunction,
     	    	  GridFunctionType& rhs,
     	    	  const PointType& delta,
     	    	  RealType omega);
+    //-------------------------------------------------------------------------------
 
+    /*! Constructor
+    * (does not have to do anything)
+    */
     Solver::Solver(void);
+    //-------------------------------------------------------------------------------
 
+    /*! Destructor
+     */
     Solver::~Solver();
 
 private:
