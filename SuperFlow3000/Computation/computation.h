994f17238f4c14a6042291e15da904c06c1c2e4f
Aaron <Aaron@Aaron-PC>
2013-11-13 15:10:57 +0100

    computation.h um Punkt 4. Predeclarations erweitert.

--- SuperFlow3000/Computation/computation.h
+++ SuperFlow3000/Computation/computation.h
@@ -25,8 +25,36 @@ public:
 								GridFunctionType* u, GridFunctionType* v,
 								GridFunctionType& gx, GridFunctionType& gy,
 								const PointType& h, RealType deltaT);
-};
 
+	/*! @brief Function to set the boundary values for u, the velocities in x-direction.
+	 * First implementation: only no-flow boundaries.
+	 *  @param velocity_x is a GridFunction-Object, containing all discretization points.
+	 */
+	void setBoundaryU(GridFunction velocity_x);
+
+	/*! @brief Function to set the boundary values for v, the velocities in y-direction.
+	 * First implementation: only no-flow boundaries.
+	 *  @param velocity_y is a GridFunction-Object, containing all discretization points.
+	 */
+	void setBoundaryV(GridFunction velocity_y);
+
+	/*! @brief Function to set the boundary values for the pressure p.
+	 *  @param pressure is a GridFunction-Object, containing all discretization points.
+	 */
+	void setBoundaryP(GridFunction pressure);
+
+	/*! @brief Function to set the boundary values for F (12).
+	 *  @param f is a GridFunction-Object, containing F for all discretization points.
+	 */
+	void setBoundaryF(GridFunction f);
+
+	/*! @brief Function to set the boundary values for G.
+	 *  @param g is a GridFunction-Object, containing G for all discretization points.
+	 */
+	void setBoundaryG(GridFunction g);
+
+
+};
 
 
 #endif /* COMPUTATION_H_ */
