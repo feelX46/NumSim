f18834372f7a76f4bfdea9f9697d3998673cde3e
felix <felix@localhost>
2013-11-13 17:40:05 +0000

    Apply Stencil Operator funktioniert (Achtung: alle Matritzen haben oben links (0,0)

--- SuperFlow3000/Stencil/stencil.cpp
+++ SuperFlow3000/Stencil/stencil.cpp
@@ -10,7 +10,10 @@
 
 
 Stencil::Stencil(int stencilwidth_input, PointType& h_input) : stencilwidth(stencilwidth_input), h(h_input){
-	stencil = new RealType*[stencilwidth_input];
+	 stencil = new RealType*[stencilwidth];
+	 for (int i = 0; i < stencilwidth; i++){
+		 stencil[i] = new RealType [stencilwidth];
+	 }
 }
 
 
@@ -20,29 +23,48 @@ void Stencil::ApplyStencilOperator(const MultiIndexType& gridreadbegin,
 		const MultiIndexType& gridwriteend,
 		const GridFunctionType sourcegridfunction,
 		GridFunction imagegridfunction){
-	for (IndexType i=gridreadbegin[0]; i<gridreadend[0]; i++){
-	    	for (IndexType j=gridreadbegin[1]; j<gridreadend[1];  j++){
-	    		// fuer jede Ableitung wird die 3x3 Stencil Matrix angewendet
-	    				for(IndexType k=0; k<3;k++){
-	    					for(IndexType l=0;l<3;l++){
-	    						imagegridfunction.SetGridFunction(i,j,sourcegridfunction[i-k-1][j-l-1]*stencil[k][l]);
-	    					}
-	    				}
-	    	}
-	    }
+
+
+	// Berechne die Ableitungen
+	// (0,0) ist bei allen allen drei Matrtzen (Stencil, sourcegrid, imagegrid) oben links
+	RealType tmp;
+	for (IndexType i=gridwritebegin[0]+1; i<gridwriteend[0]; i++){
+		for (IndexType j=gridwritebegin[1]+1; j<gridwriteend[1]; j++){
+			tmp = 0.0;
+			for(IndexType k=0; k<3; k++){
+				for(IndexType l=0; l<3; l++){
+					tmp = tmp + sourcegridfunction[i+k-1][j+l-1]*stencil[l][k];
+				}
+			}
+			imagegridfunction.SetGridFunction(i,j,tmp);
+	 	}
+	}
+
+	// Da die Groesse der Ableitungsmatrix so gross ist, wie die eigentliche Matrix,
+	// werden die Ableitungsraender auf 0 gesetzt
+	for (IndexType i = gridreadbegin[0]; i < gridreadend[0]+1;i++){
+		imagegridfunction.SetGridFunction(0,i,0.0);
+		imagegridfunction.SetGridFunction(gridreadend[0],i,0.0);
+	}
+	for (IndexType j = gridreadbegin[1]; j < gridreadend[1]+1; j++) {
+		imagegridfunction.SetGridFunction(j,0,0.0);
+		imagegridfunction.SetGridFunction(j,gridreadend[0],0.0);
+	}
+
+
 
 }
 
 void Stencil::setFxxStencil() {
-	stencil[-1][-1] = 0;
-	stencil[0][-1] = 0;
-	stencil[1][-1] = 0;
-	stencil[-1][0] = 1/(h[0]*h[0]);
-	stencil[0][0] = -2/(h[0]*h[0]);
-	stencil[1][0] = 1/(h[0]*h[0]);
-	stencil[-1][1] = 0;
-	stencil[0][1] = 0;
-	stencil[1][1] = 0;
+	stencil[0][0] = 0;
+	stencil[1][0] = 0;
+	stencil[2][0] = 0;
+	stencil[0][1] = 1/(h[0]*h[0]);
+	stencil[1][1] = -2/(h[0]*h[0]);
+	stencil[2][1] = 1/(h[0]*h[0]);
+	stencil[0][2] = 0;
+	stencil[1][2] = 0;
+	stencil[2][2] = 0;
 }
 
 void Stencil::setFyyStencil() {
